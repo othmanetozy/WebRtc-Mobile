22f8c85b9bf6a26c25cf5c6c0552ba765d969378
othmane tozy <tozyothmane123@gmail.com>
2024-03-11 11:57:31 +0000

    fix resolutions UI & adding switch

--- lib/icons/util/Mydevice.page.dart
+++ lib/icons/util/Mydevice.page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
@@ -17,10 +18,10 @@ class Mydevice extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(11.0),
       child: Container(
         decoration: BoxDecoration(color: Colors.grey[100],
-          borderRadius : BorderRadius.circular(24),
+          borderRadius : BorderRadius.circular(10),
         ),
         child: Column(
           children: [
@@ -30,7 +31,11 @@ class Mydevice extends StatelessWidget {
             ),
             
             //name the device 
-            Text(DeviceUsine)
+            Text(DeviceUsine),
+            CupertinoSwitch
+              (value: false,
+                onChanged: (value) {}
+            )
           ],
         ),
       ),
