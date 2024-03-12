7570c1377de5f1d73491ecb643782af05f5c854b
Tahaaznag <tahaaznag111@gmail.com>
2024-03-12 09:13:03 +0100

    Video Call

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -2,17 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:testflutter/icons/util/Mydevice.page.dart';
 
 class HomePage extends StatelessWidget {
-
   final double horizantalPading = 40;
   final double verticalPading = 25;
 
   // list
 
   List myDevices = [
-    ["usine","lib/icons/usine.png",true],
-    ["Réglages  ","lib/icons/reglages.png",false],
-    ["industry","lib/icons/industry.png",false],
-    ["industrie","lib/icons/industrie.png",false],
+    ["usine", "lib/icons/usine.png", true],
+    ["Réglages  ", "lib/icons/reglages.png", false],
+    ["industry", "lib/icons/industry.png", false],
+    ["industrie", "lib/icons/industrie.png", false],
   ];
 
   @override
@@ -26,8 +25,8 @@ class HomePage extends StatelessWidget {
           children: [
             Padding(
               padding: EdgeInsets.symmetric(
-                  horizontal:horizantalPading,
-                  vertical: verticalPading,
+                horizontal: horizantalPading,
+                vertical: verticalPading,
               ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -45,49 +44,48 @@ class HomePage extends StatelessWidget {
                 ],
               ),
             ),
-            const SizedBox(height: 20,),
-
+            const SizedBox(
+              height: 20,
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 40),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text("Bienvenue", style: TextStyle(color: Colors.black)),
-                  Text("Berger-Levrault",style: TextStyle(fontSize: 40,color: Colors.red),)
+                  Text(
+                    "Berger-Levrault",
+                    style: TextStyle(fontSize: 40, color: Colors.red),
+                  )
                 ],
               ),
             ),
             Padding(
-              padding:EdgeInsets.symmetric(horizontal:horizantalPading),
-              child: Text("Industry GMAO")
-            ),
+                padding: EdgeInsets.symmetric(horizontal: horizantalPading),
+                child: Text("Industry GMAO")),
             Expanded(
               child: GridView.builder(
                 itemCount: myDevices.length,
                 padding: EdgeInsets.all(25),
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2
-                ),
+                    crossAxisCount: 2),
                 itemBuilder: (context, index) {
                   // Here you should return the widget for each item in the grid
                   return Mydevice(
                       DeviceUsine: myDevices[index][0],
                       iconDevice: myDevices[index][1],
-                      powerOn: myDevices[index][2]
-                  );
+                      powerOn: myDevices[index][2]);
                 },
               ),
             )
           ],
         ),
       ),
-
-
-      drawer:   Drawer(
+      drawer: Drawer(
         child: Column(
-          children:[
+          children: [
             DrawerHeader(
-              child:Row(
+              child: Row(
                 children: [
                   CircleAvatar(
                     backgroundImage: AssetImage("images/img.png"),
@@ -99,56 +97,87 @@ class HomePage extends StatelessWidget {
                   gradient: LinearGradient(
                       colors: [Theme.of(context).primaryColor, Colors.red])),
             ),
-
-            ListTile(// list item
+            ListTile(
+              // list item
               leading: Icon(
-                Icons.home, color: Theme.of(context).primaryColor,),
+                Icons.home,
+                color: Theme.of(context).primaryColor,
+              ),
               trailing: Icon(Icons.arrow_forward_ios_sharp),
-              onTap: (){
+              onTap: () {
                 Navigator.pushNamed(context, "/");
               },
               title: Text(
                 "Home",
                 style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                  fontFamily: "Rubik-Italic",),
+                      fontFamily: "Rubik-Italic",
+                    ),
               ),
             ),
-            ListTile(// list item
+            ListTile(
+              // list item
               leading: Icon(
-                Icons.stars, color: Theme.of(context).primaryColor,),
+                Icons.video_call,
+              ),
               trailing: Icon(Icons.arrow_forward_ios_sharp),
-              onTap: (){
+              onTap: () {
+                Navigator.pushNamed(context, "/videocall");
+              }, // click
+              title: Text(
+                "Video Call",
+                style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                      fontFamily: "Rubik-Italic",
+                    ),
+              ),
+            ),
+            ListTile(
+              // list item
+              leading: Icon(
+                Icons.stars,
+                color: Theme.of(context).primaryColor,
+              ),
+              trailing: Icon(Icons.arrow_forward_ios_sharp),
+              onTap: () {
                 Navigator.pushNamed(context, "/review");
-              },  // click
+              }, // click
               title: Text(
                 "Review",
                 style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                  fontFamily: "Rubik-Italic",),
+                      fontFamily: "Rubik-Italic",
+                    ),
               ),
             ),
-            ListTile(// list item
+            ListTile(
+              // list item
               leading: Icon(
-                Icons.supervised_user_circle, color: Theme.of(context).primaryColor,),
+                Icons.supervised_user_circle,
+                color: Theme.of(context).primaryColor,
+              ),
               trailing: Icon(Icons.arrow_forward_ios_sharp),
-              onTap: (){
+              onTap: () {
                 Navigator.pushNamed(context, "/consulting");
               },
               title: Text(
                 "Consulting",
                 style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                  fontFamily: "Rubik-Italic",),
+                      fontFamily: "Rubik-Italic",
+                    ),
               ),
             ),
             ListTile(
-              leading: Icon(Icons.production_quantity_limits, color: Theme.of(context).primaryColor,),
+              leading: Icon(
+                Icons.production_quantity_limits,
+                color: Theme.of(context).primaryColor,
+              ),
               trailing: Icon(Icons.arrow_forward_ios_sharp),
-              onTap: (){
+              onTap: () {
                 Navigator.pushNamed(context, "/products");
               },
               title: Text(
                 "Products",
                 style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                  fontFamily: "Rubik-Italic",),
+                      fontFamily: "Rubik-Italic",
+                    ),
               ),
             ),
           ],
---
+++ lib/pages/videocall.page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class VideocallPage extends StatelessWidget {
+  const VideocallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text("video"),
+      ),
+    );
+  }
+}
