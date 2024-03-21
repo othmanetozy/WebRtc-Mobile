1f839ae3320c3b719c12398c17260524c37ad713
othmane tozy <tozyothmane123@gmail.com>
2024-03-21 10:16:49 +0000

    creating Server signalization

--- server/server.js
+++ server/server.js
@@ -1,3 +1,35 @@
 const app = require('express')();
-const {server} = require('socket.io');
+const {Server} = require('socket.io');
 
+
+async function server() {
+    const http = require('http').createServer(app);
+    const io = new Server(http,{transports: ['websocket.io']});
+    const roomName = "test";
+
+
+    io.on("connection",(socket)=>{
+        socket.on("join",() =>{
+            socket.join(roomName);
+            socket.to(roomName).emit("joindre la reunion")
+        });
+
+
+        socket.on("offer",(offer) =>{
+            socket.to(roomName).emit("offer",offer);
+        });
+        
+        socket.on("answer",(answer)=>{
+            socket.to(roomName).emit("answer",answer);
+        });
+        
+        socket.on("ice",(ice)=>{
+            socket.to(roomName).emit("ice",ice);
+        });
+    });
+
+    http.listen(3001,()=>console.log('server is runnig'));
+}
+
+
+server();
