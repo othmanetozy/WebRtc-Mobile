5010881d263c644c8dd81e041b72537ccb29aae2
othmane tozy <tozyothmane123@gmail.com>
2024-04-22 09:18:28 +0100

    update

--- lib/pages/videocall.page.dart
+++ lib/pages/videocall.page.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:html';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
--- server/server.js
+++ server/server.js
@@ -2,48 +2,76 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 
-async function server() {
-  const app = express();
-  const httpServer = http.createServer(app);
-  const io = new Server(httpServer, { transports: ['websocket'] });
-  const roomName = 'test';
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { transports: ['websocket'] });
 
-  // Route pour la racine
-  app.get('/', (req, res) => {
-    res.send('Welcome to my Socket.IO server');
+const rooms = {}; // Définissez une variable pour stocker les salles
+
+// Route pour la racine
+app.get('/', (req, res) => {
+  res.send('Welcome to my Socket.IO server');
+});
+
+io.on('connection', socket => {
+  console.log(`Utilisateur connecté ${socket.id}`);
+
+  // Logique pour gérer la création d'une nouvelle salle
+  socket.on('create-room', roomId => {
+    if (!rooms[roomId]) {
+      rooms[roomId] = {
+        membres: [socket.id],
+      };
+      socket.emit('room-created', roomId);
+      console.log(`Pièce créée : ${roomId}`);
+    } else {
+      // La pièce existe déjà
+      socket.emit('room-exists');
+    }
   });
 
-  io.on('connection', (socket) => {
-    console.log('Client connected');
-
-    socket.on('join', () => {
-      console.log("Received 'join' event");
-      socket.join(roomName);
-      console.log("Joined room: " + roomName);
-      socket.to(roomName).emit('joined');
-      console.log("Emitted 'joined' event to room: " + roomName);
-    });
-
-    socket.on('offer', (offer) => {
-      socket.to(roomName).emit('offer', offer);
-    });
-
-    socket.on('answer', (answer) => {
-      socket.to(roomName).emit('answer', answer);
-    });
-
-    socket.on('ice', (ice) => {
-      socket.to(roomName).emit('ice', ice);
-    });
-    socket.on('disconnect', () => {
-      console.log('Client disconnected');
-      socket.to(roomName).emit('user disconnected');
-    });
+  // Logique pour gérer l'adhésion à une salle existante
+  socket.on('join-room', (roomId, requesterUserId) => {
+    const room = rooms[roomId];
+    if (room && room.membres.length === 1) {
+      // La salle est disponible
+      room.membres.push(requesterUserId);
+      console.log(`Utilisateur ${requesterUserId} a rejoint la salle ${roomId}`);
+      // Envoyer un message au propriétaire de la salle
+      socket.to(room.membres[0]).emit('join-request', requesterUserId);
+    } else {
+      // La salle est pleine ou n'existe pas
+      socket.emit('room-unavailable');
+    }
   });
 
-  httpServer.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+  // Logique pour gérer les offres et les réponses
+  socket.on('offer-request', data => {
+    const { fromOffer, to } = data;
+    console.log(`Transfert de la demande d'offre à : ${to}`);
+    socket.to(to).emit('offer-request', { from: socket.id, offer: fromOffer });
   });
-}
 
-server();
+  socket.on('offer-response', data => {
+    const { response, to } = data;
+    console.log(`Transfert de la réponse de l'offre à : ${to}`);
+    socket.to(to).emit('offer-response', { from: socket.id, response: response });
+  });
+
+  // Logique pour gérer les mises à jour des pairs
+  socket.on('peer-update', data => {
+    const { candidate, to } = data;
+    console.log("Mise à jour du pair");
+    socket.to(to).emit('peer-update', { from: socket.id, candidate: candidate });
+  });
+
+  // Logique pour gérer la déconnexion de l'utilisateur
+  socket.on('disconnect', () => {
+    console.log('Utilisateur déconnecté :', socket.id);
+    // Gérer la déconnexion de l'utilisateur, quitter les salles, etc.
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
